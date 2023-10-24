2a0cd3f10ff3d4fd01944bb7c0a4e4de6d7091e8
mazalea <azalea.nareswari@gmail.com>
2023-10-24 09:18:28 +0700

    initial

---
+++ app/src/main/java/com/example/ppapb9_azalea/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.ppapb9_azalea
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.ppapb9_azalea.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun replacefragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+}
